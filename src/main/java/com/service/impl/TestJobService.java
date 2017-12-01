5fd92f10ffc8c05383301f4ce47c1208bf774f5f
84548 <845486067@>
2017-12-01 15:31:29 +0800

    测试用例

--- src/main/java/com/service/impl/TestJobService.java
+++ src/main/java/com/service/impl/TestJobService.java
@@ -1,25 +1,22 @@
 package com.service.impl;
 
 import com.dao.TimerJobDao;
+import com.service.BaseJobService;
 import com.util.CommonUtils;
 import org.quartz.DisallowConcurrentExecution;
-import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @DisallowConcurrentExecution
-public class TestJobService implements Job {
+public class TestJobService implements BaseJobService {
     private static final Logger logger = LoggerFactory.getLogger(TestJobService.class);
     @Autowired
     private TimerJobDao timerJobDao;
 
-    @Override
-    @Transactional
     public void execute(JobExecutionContext context) {
         logger.info("测试用例执行成功，时间:{}", CommonUtils.getCurrentTime());
         timerJobDao.selectByPrimaryKey("777");
